d93e84e8ef8d46c6fe5fcbf5bed87f111268bc17
Jack Kearney <carrot1500@gmail.com>
2024-03-10 16:30:46 -0500

    Add PPO for learning

--- src/gin_rummy/cli/rummy_agent_training.py
+++ src/gin_rummy/cli/rummy_agent_training.py
@@ -2,7 +2,7 @@ import click
 
 from pathlib import Path
 
-from gin_rummy.rl.training import train_agent
+from gin_rummy.rl.training import train_agent, RF
 from gin_rummy.players import CardPointPlayer, RummyAgent
 
 
@@ -11,20 +11,28 @@ from gin_rummy.players import CardPointPlayer, RummyAgent
 @click.argument("output", type=Path)
 @click.option("--games", type=int, default=500)
 @click.option("--epochs", type=int, default=10)
+@click.option("--algorithm", type=str, default=RF)
 @click.option("--ignore-critic", is_flag=True, default=False)
 @click.option("--lr", type=float, default=0.1)
 @click.option("--lr-warmup-steps", type=int, default=0)
-@click.option("--unscaled-rewards", is_flag=True, default=False)
+@click.option("--no-minibatch-scaling", is_flag=True, default=False)
+@click.option("--minibatch-size", type=int, default=512)
+@click.option("--max-minibatches", type=int, default=8)
+@click.option("--epsilon", type=float, default=0.2)
 @click.option("--workers", type=int, default=4)
 def main(
     network: str,
     output: Path,
     games: int = 500,
     epochs: int = 10,
+    algorithm: str = RF,
     ignore_critic: bool = False,
     lr: float = 0.1,
     lr_warmup_steps: int = 0,
-    unscaled_rewards: bool = False,
+    no_minibatch_scaling: bool = False,
+    minibatch_size: int = 512,
+    max_minibatches: int = 8,
+    epsilon: float = 0.2,
     workers: int = 4
 ):
     agent = RummyAgent(network=network)
@@ -35,10 +43,14 @@ def main(
         games,
         agent,
         opponent_pool,
+        algorithm=algorithm.upper(),
         ignore_critic=ignore_critic,
         lr=lr,
         lr_warmup_steps=lr_warmup_steps,
-        unscaled_rewards=unscaled_rewards,
+        scale_minibatch=not no_minibatch_scaling,
+        minibatch_size=minibatch_size,
+        max_minibatches=max_minibatches,
+        epsilon=epsilon,
         output=output,
         always_save_checkpoint=False,
         num_workers=workers
--- src/gin_rummy/rl/training.py
+++ src/gin_rummy/rl/training.py
@@ -2,19 +2,25 @@
 Useful references:
 
 https://www.tensorflow.org/tutorials/reinforcement_learning/actor_critic
+https://karpathy.github.io/2016/05/31/rl/
 """
 
 import torch
+
 from torch.utils.data import DataLoader
 from time import time
 from pathlib import Path
-
 from typing import List, Union
+from copy import deepcopy
 
 from gin_rummy.gameplay.game_manager import BasePlayer
 from gin_rummy.gameplay.playouts import run_playouts
 from gin_rummy.players.agent import RummyAgent
 from gin_rummy.rl.policy_networks import PolicyNetwork
+
+
+RF = "REINFORCE"
+PPO = "PPO"
 
 
 def save_checkpoint(
@@ -44,21 +50,32 @@ def train_agent(
     n_games: int,
     player: RummyAgent,
     opponent_pool: Union[List[BasePlayer], BasePlayer],
+    algorithm: str = RF,
     ignore_critic: bool = False,
     lr: float = 0.1,
     lr_warmup_steps: int = 0,
-    unscaled_rewards: bool = False,
+    scale_minibatch: bool = True,
+    minibatch_size: int = 512,
+    max_minibatches: int = 8,
+    epsilon: float = 0.2,
     output: Path = None,
     always_save_checkpoint: bool = False,
     num_workers: int = 4,
 ) -> BasePlayer:
     assert hasattr(player, "model"), "player must be using a model for training!"
+    assert algorithm in {"REINFORCE", "PPO"}, f"unknown RL training algorithm {algorithm}"
 
     optimizer = torch.optim.Adam(player.model.parameters(), lr=0.1)
     critic_loss_function = torch.nn.BCEWithLogitsLoss()
 
     if output:
         output.mkdir(exist_ok=True, parents=True)
+
+    if algorithm == PPO:
+        ref_model = deepcopy(player.model)
+        ref_model.eval()
+    else:
+        ref_model = None
 
     win_rates = []
     best_win_rate = 0.0
@@ -88,15 +105,19 @@ def train_agent(
         for pg in optimizer.param_groups:
             pg["lr"] = elr
 
+        if algorithm == "REINFORCE":
+            minibatch_size, max_minibatches = len(dataset), 1
+
         dl = DataLoader(
             dataset,
-            batch_size=len(dataset),
+            batch_size=minibatch_size,
             shuffle=True,
             num_workers=num_workers,
         )
 
-        # Basic REINFORCE algorithm to start
+        # Run REINFORCE on entire dataset, or PPO on a maximum number of minibatches
         player.model.train()
+        minibatches = 0
         for state, ostate, action, reward in dl:
             optimizer.zero_grad()
 
@@ -108,15 +129,31 @@ def train_agent(
                 with torch.no_grad():
                     reward = reward - torch.exp(logr)
 
-            # TODO: make configurable
-            if not unscaled_rewards:
+            if scale_minibatch:
                 reward = (reward - reward.mean()) / (reward.std() + 1E-8)
             logp_a = logp.gather(-1, action.unsqueeze(-1)).squeeze()
-            actor_loss = -(reward * logp_a).mean()
+
+            if algorithm == RF:
+                actor_loss = -(reward * logp_a).mean()
+            elif algorithm == PPO:
+                logp_ref, _ = ref_model(state)
+                logp_a_ref = logp_ref.gather(-1, action.unsqueeze(-1)).squeeze()
+                prob_ratio = torch.exp(logp_a - logp_a_ref)
+                clamped_prob_ratio = torch.clamp(prob_ratio, 1 - epsilon, 1 + epsilon)
+                actor_loss = -torch.minimum(prob_ratio * reward, clamped_prob_ratio * reward).mean()
+            else:
+                raise NotImplementedError
 
             loss = actor_loss + critic_loss
             loss.backward()
             optimizer.step()
+
+            minibatches += 1
+            if minibatches >= max_minibatches:
+                break
+
+        if ref_model is not None:
+            ref_model.load_state_dict(player.model.state_dict())
 
         print(f"Epoch completed in {time() - st:.2f}s")
 
